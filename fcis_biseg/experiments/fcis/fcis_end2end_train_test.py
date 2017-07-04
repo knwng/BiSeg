25cb3f7a88d53a2529ea32af975c6977f962d985
knwng <ken.qrose@gmail.com>
2017-07-04 10:36:39 +0800

    biseg rm submodule

---
+++ fcis_biseg/experiments/fcis/fcis_end2end_train_test.py
@@ -0,0 +1,19 @@
+import os
+import sys
+os.environ['PYTHONUNBUFFERED'] = '1'
+os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
+os.environ['MXNET_ENABLE_GPU_P2P'] = '0'
+this_dir = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(this_dir, '..', '..', 'fcis'))
+
+# import train_end2end
+import biseg_train
+import test
+
+if __name__ == "__main__":
+    biseg_train.main()
+    test.main()
+
+
+
+
